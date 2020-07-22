aa52ab4869ac1dfba758182b65965380da7db848
trungvd <trungvd14293@gmail.com>
2020-07-22 17:50:35 +0700

    22.07.2020

---
+++ Project_Frist.Data/Configurations/AppConfigConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project_Frist.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Frist.Data.Configurations
+{
+    public class AppConfigConfiguration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            builder.ToTable("AppConfigs");
+            builder.HasKey(x => x.Key);
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
