78dfaead505756e276077f225e0f50eb3768cd76
trungvd <trungvd14293@gmail.com>
2020-08-10 17:49:41 +0700

    Method

---
+++ Project_Frist.Application/Catalog/Products/Dtos/Manage/GetManageProductPagingRequest.cs
@@ -0,0 +1,13 @@
+﻿using Project_Frist.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Frist.Application.Catalog.Products.Dtos.Manage
+{
+    public class GetManageProductPagingRequest : PagingRequestBase
+    {
+        public string Keyword { get; set; }
+        public List<int> CategoryIds { get; set; }
+    }
+}
---
+++ Project_Frist.Application/Catalog/Products/Dtos/Public/GetProductPagingRequest.cs
@@ -0,0 +1,12 @@
+﻿using Project_Frist.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Frist.Application.Catalog.Products.Dtos.Public
+{
+    public class GetProductPagingRequest: PagingRequestBase
+    {
+        public int? CategoryId { get; set; }
+    }
+}
