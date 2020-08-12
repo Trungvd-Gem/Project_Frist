7251b8e378e9aa20f5d3fa7db4fe7777edb4766a
trungvd <trungvd14293@gmail.com>
2020-08-12 18:39:47 +0700

    Login_app

---
+++ Project_Frist.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Project_Frist.ViewModels.System.Users;
+
+namespace Project_Frist.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
